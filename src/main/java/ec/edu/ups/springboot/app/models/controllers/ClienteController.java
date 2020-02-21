e059c76ca153b6353d62d034f00358f42bf5cba6
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-02-20 20:59:55 -0500

    editar cliente y uso de SessionAtribute

--- src/main/java/ec/edu/ups/springboot/app/models/controllers/ClienteController.java
+++ src/main/java/ec/edu/ups/springboot/app/models/controllers/ClienteController.java
@@ -9,26 +9,30 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import ec.edu.ups.springboot.app.models.dao.IClienteDao;
 import ec.edu.ups.springboot.app.models.entity.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 
 	@Autowired
 	@Qualifier("clientedaojpa")
 	private IClienteDao clienteDao;
-	
+
 	@RequestMapping(value = "listar", method = RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
-	
+
 	/*
 	 * Este metodo solo llama al formulario o solo renderiza con un titulo
 	 */
@@ -36,19 +40,33 @@ public class ClienteController {
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
+		model.put("btn", "Crear Cliente");
 		model.put("titulo", "Formulario de cliente");
 		return "form";
 	}
-	
+
 	@RequestMapping(value = "form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
-		if(result.hasErrors()) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario clientes");
 			return "form";
 		}
 		clienteDao.save(cliente);
+		status.setComplete();																				//session atribute elimana el objeto cliente
 		return "redirect:listar";
 	}
-	
-	
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Model model) {
+		if(id > 0) {
+			Cliente cliente = clienteDao.findOne(id);
+			model.addAttribute("cliente", cliente);
+			model.addAttribute("btn", "Editar Cliente");
+			model.addAttribute("titulo", "Editar cliente");
+		} else {
+			return "listar";
+		}
+		return "form";
+	}
+
 }
--- src/main/java/ec/edu/ups/springboot/app/models/dao/ClienteDao.java
+++ src/main/java/ec/edu/ups/springboot/app/models/dao/ClienteDao.java
@@ -15,15 +15,23 @@ public class ClienteDao implements IClienteDao {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
 		return em.createQuery("from Cliente").getResultList();
 	}
-	
+
 	@Transactional
 	public void save(Cliente cliente) {
-		em.persist(cliente);
+		if (cliente.getId() != null && cliente.getId() > 0) {
+			em.merge(cliente);
+		} else {
+			em.persist(cliente);
+		}
+	}
+
+	public Cliente findOne(Long id) {
+		return em.find(Cliente.class, id);
 	}
 
 }
